c82123d6cdd78b2b169f30749eb94bb076555b19
Lawrence Matacena <lawrence.matacena@gmail.com>
2014-10-14 18:05:53 -0400

    missed Username field in QBittorrentSettings.cs

--- src/NzbDrone.Core/Download/Clients/qBittorrent/QBittorrentSettings.cs
+++ src/NzbDrone.Core/Download/Clients/qBittorrent/QBittorrentSettings.cs
@@ -32,19 +32,22 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
         [FieldDefinition(1, Label = "Port", Type = FieldType.Textbox)]
         public Int32 Port { get; set; }
 
-        [FieldDefinition(2, Label = "Password", Type = FieldType.Password)]
+        [FieldDefinition(2, Label = "Username", Type = FieldType.Textbox)]
+        public String Username { get; set; }
+
+        [FieldDefinition(3, Label = "Password", Type = FieldType.Password)]
         public String Password { get; set; }
 
-        [FieldDefinition(3, Label = "Category", Type = FieldType.Textbox)]
+        [FieldDefinition(4, Label = "Category", Type = FieldType.Textbox)]
         public String TvCategory { get; set; }
 
-        [FieldDefinition(4, Label = "Recent Priority", Type = FieldType.Select, SelectOptions = typeof(QBittorrentPriority), HelpText = "Priority to use when grabbing episodes that aired within the last 14 days")]
+        [FieldDefinition(5, Label = "Recent Priority", Type = FieldType.Select, SelectOptions = typeof(QBittorrentPriority), HelpText = "Priority to use when grabbing episodes that aired within the last 14 days")]
         public Int32 RecentTvPriority { get; set; }
 
-        [FieldDefinition(5, Label = "Older Priority", Type = FieldType.Select, SelectOptions = typeof(QBittorrentPriority), HelpText = "Priority to use when grabbing episodes that aired over 14 days ago")]
+        [FieldDefinition(6, Label = "Older Priority", Type = FieldType.Select, SelectOptions = typeof(QBittorrentPriority), HelpText = "Priority to use when grabbing episodes that aired over 14 days ago")]
         public Int32 OlderTvPriority { get; set; }
 
-        [FieldDefinition(6, Label = "Use SSL", Type = FieldType.Checkbox)]
+        [FieldDefinition(7, Label = "Use SSL", Type = FieldType.Checkbox)]
         public Boolean UseSsl { get; set; }
 
         public ValidationResult Validate()
