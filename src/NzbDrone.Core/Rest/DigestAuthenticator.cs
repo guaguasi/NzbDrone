1b9c7fef1ff1dc1c7a41bdd1d5fb8826cf5b7afe
Lawrence Matacena <lawrence.matacena@gmail.com>
2014-10-15 22:58:28 -0400

    added basic digest authentication for RestSharp
    By Uwe Keim:
    http://blog.magerquark.de/digest-authentication-with-restsharp/
    If query string values cause authentication issues in the future, see:
    http://www.ifjeffcandoit.com/2013/05/16/digest-authentication-with-restsharp/

---
+++ src/NzbDrone.Core/Rest/DigestAuthenticator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using RestSharp;
+
+// By Uwe Keim: http://blog.magerquark.de/digest-authentication-with-restsharp/
+// If query string values cause authentication issues in the future, see: http://www.ifjeffcandoit.com/2013/05/16/digest-authentication-with-restsharp/
+
+namespace NzbDrone.Core.Rest
+{
+
+    public class DigestAuthenticator : IAuthenticator
+    {
+        private readonly String _username;
+        private readonly String _password;
+
+        public DigestAuthenticator(String username, String password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        public void Authenticate(IRestClient client, IRestRequest request)
+        {
+            request.Credentials = new NetworkCredential(_username, _password);
+        }
+    }
+}
